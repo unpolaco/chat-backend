c33ee9a31b6d38a9d9dc8c903c76583931fae6fd
Michal Borowski <unpolacoo@gmail.com>
2022-05-23 23:45:51 +0200

    Create Chat, ChatUser and Message model and migrations

---
+++ database/seeders/20220523212912-chats.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const models = require('../../models')
+const User = models.User
+const Chat = models.Chat
+const ChatUser = models.ChatUser
+const Message = models.Message
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+const users = await User.findAll({limit: 2})
+const chat = await Chat.create()
+await ChatUser.bulkCreate([
+  {
+  chatId: chat.id,
+  userId: users[0].id
+  },
+  {
+  chatId: chat.id,
+  userId: users[1].id
+  },
+])
+
+await Message.bulkCreate([
+  {
+    message: 'Hello friend',
+    chatId: chat.id,
+    fromUserId: users[0].id
+  },
+  {
+    message: 'Hey buddy',
+    chatId: chat.id,
+    fromUserId: users[1].id
+  },
+  {
+    message: 'Where are you?',
+    chatId: chat.id,
+    fromUserId: users[1].id
+  },
+])
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
