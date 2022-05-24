ffd18434eb509381ccc2fa3817e3cc65e8ab15f5
Michal Borowski <unpolacoo@gmail.com>
2022-05-24 15:03:15 +0200

    Create chat messages with pagination

---
+++ router/chat.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const { index, create, messages } = require("../controllers/chatController");
+const { auth } = require("../middleware/auth");
+
+router.get("/", [auth], index);
+router.get("/messages", [auth], messages);
+router.post("/create", [auth], create);
+
+module.exports = router;
