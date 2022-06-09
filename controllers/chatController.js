ce2315a1a9103a190323fb64875921494529f566
Michal Borowski <unpolacoo@gmail.com>
2022-06-10 00:02:01 +0200

    Create deleteChat endpoint

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,6 +1,7 @@
 const models = require("../models");
-const { Op, UnknownConstraintError } = require("sequelize");
+const { Op } = require("sequelize");
 const { sequelize } = require("../models");
+const e = require("express");
 const User = models.User;
 const Chat = models.Chat;
 const ChatUser = models.ChatUser;
@@ -25,6 +26,11 @@ exports.index = async (req, res) => {
           },
           {
             model: Message,
+            include: [
+              {
+                model: User
+              }
+            ],
             limit: 20,
             order: [["id", "DESC"]],
           },
@@ -110,26 +116,52 @@ exports.create = async (req, res) => {
 };
 
 exports.messages = async (req, res) => {
-    const limit = 10
-    const page = req.query.page || 1
-    const offset = page > 1 ? page * limit : 0
+  const limit = 10;
+  const page = req.query.page || 1;
+  const offset = page > 1 ? page * limit : 0;
 
-    const messages = await Message.findAndCountAll({
-        where: {
-            chatId: req.query.id
+  const messages = await Message.findAndCountAll({
+    where: {
+      chatId: req.query.id,
+    },
+    include: [
+      {
+        model: User
+      }
+    ],
+    limit,
+    offset,
+  });
+  const totalPages = Math.ceil(messages.count / limit);
+  if (page > totalPages) return res.json({ data: { messages: [] } });
+
+  const result = {
+    messages: messages.rows,
+    pagination: {
+      page,
+      totalPages,
+    },
+  };
+  return res.json(result);
+};
+
+exports.deleteChat = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const chat = await Chat.findOne({
+      where: {
+        id,
+      },
+      include: [
+        {
+          model: User,
         },
-        limit,
-        offset
-    })
-    const totalPages = Math.ceil(messages.count / limit)
-    if (page > totalPages) return res.json({ data: {messages: []}})
-    
-    const result = {
-        messages: messages.rows,
-        pagination: {
-            page, 
-            totalPages
-        }
-    }
-    return res.json(result)
-}
+      ],
+    });
+    const notifyUsers = chat.Users.map((user) => user.id);
+    await chat.destroy();
+    return res.json({ chatId: id, notifyUsers });
+  } catch (e) {
+    return res.status(500).json({ status: "Error", message: e.message });
+  }
+};
--- router/chat.js
+++ router/chat.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-const { index, create, messages } = require("../controllers/chatController");
+const { index, create, messages, deleteChat } = require("../controllers/chatController");
 const { auth } = require("../middleware/auth");
 
 router.get("/", [auth], index);
 router.get("/messages", [auth], messages);
 router.post("/create", [auth], create);
+router.delete("/:id", [auth], deleteChat);
 
 module.exports = router;
