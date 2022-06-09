1d97ebab2fc115bd637b2f25022b19038d18ed6d
Michal Borowski <unpolacoo@gmail.com>
2022-06-10 00:18:54 +0200

    Create socket server

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const config = require('./config/app')
 const router = require('./router')
 const cors = require('cors')
 const app = express();
+const http = require('http')
 
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
@@ -10,7 +11,13 @@ app.use(express.json())
 app.use(router)
 app.use(express.static(__dirname + '/public'))
 app.use(express.static(__dirname + '/uploads'))
+
 const port = config.appPort;
-app.listen(port, () => {
+
+const server = http.createServer(app)
+const SocketServer = require('./socket')
+SocketServer(server)
+
+server.listen(port, () => {
     console.log(`Running! on port ${port}`);
 });
